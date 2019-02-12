34006f851856752a949fd80b49f83ce2a0d06b1c
Matt Hilton <matt@mjhilton.net>
2019-02-13 09:59:46 +1100

    Remove chocolatey and git stuff, add Outlook signature symlink

--- New-Machine.ps1
+++ New-Machine.ps1
@@ -16,74 +16,6 @@ if (-not (Test-Path $PROFILE)) {
     New-Item $PROFILE -Force
 }
 
-Write-Progress -Activity "Ensuring Chocolatey is available"
-$null = Get-PackageProvider -Name chocolatey
-
-Write-Progress -Activity "Ensuring Chocolatey is trusted"
-if (-not ((Get-PackageSource -Name chocolatey).IsTrusted)) {
-    Set-PackageSource -Name chocolatey -Trusted
-}
-
-@(
-    "GoogleChrome",
-    "git.install",
-    "visualstudiocode",
-    "fiddler",
-    "snagit"
-) | % {
-    Write-Progress -Activity "Installing $_"
-    Install-Package -Name $_ -ProviderName chocolatey
-}
-
-Write-Progress -Activity "Setting git identity"
-$userName = (Get-WmiObject Win32_Process -Filter "Handle = $Pid").GetRelated("Win32_LogonSession").GetRelated("Win32_UserAccount").FullName
-Write-Verbose "Setting git user.name to $userName"
-git config --global user.name $userName
-# This seems to the be MSA that was first used during Windows setup
-$userEmail = (Get-WmiObject -Class Win32_ComputerSystem).PrimaryOwnerName
-Write-Verbose "Setting git user.email to $userEmail"
-git config --global user.email $userEmail
-
-Write-Progress -Activity "Setting git push behaviour to squelch the 2.0 upgrade message"
-if ((& git config push.default) -eq $null) {
-    git config --global push.default simple
-}
-
-Write-Progress -Activity "Setting git aliases"
-git config --global alias.st "status"
-git config --global alias.co "checkout"
-git config --global alias.df "diff"
-git config --global alias.lg "log --graph --oneline --decorate"
-
-Write-Progress -Activity "Setting VS Code as the Git editor"
-git config --global core.editor "code --wait"
-
-Write-Progress -Activity "Installing PoshGit"
-Install-Module posh-git -Scope CurrentUser
-Add-PoshGitToProfile
-
-Write-Progress -Activity "Enabling Office smileys"
-if (Test-Path HKCU:\Software\Microsoft\Office\16.0) {
-    if (-not (Test-Path HKCU:\Software\Microsoft\Office\16.0\Common\Feedback)) {
-        New-Item HKCU:\Software\Microsoft\Office\16.0\Common\Feedback -ItemType Directory
-    }
-    Set-ItemProperty -Path HKCU:\Software\Microsoft\Office\16.0\Common\Feedback -Name Enabled -Value 1
-}
-else {
-    Write-Warning "Couldn't find a compatible install of Office"
-}
-
-Write-Progress -Activity "Disabling Outlook notifications"
-if (Test-Path HKCU:\Software\Microsoft\Office\16.0\Outlook\Preferences) {
-    Set-ItemProperty -Path HKCU:\Software\Microsoft\Office\16.0\Outlook\Preferences -Name ChangePointer -Value 0
-    Set-ItemProperty -Path HKCU:\Software\Microsoft\Office\16.0\Outlook\Preferences -Name NewmailDesktopAlerts -Value 0
-    Set-ItemProperty -Path HKCU:\Software\Microsoft\Office\16.0\Outlook\Preferences -Name PlaySound -Value 0
-    Set-ItemProperty -Path HKCU:\Software\Microsoft\Office\16.0\Outlook\Preferences -Name ShowEnvelope -Value 0
-}
-else {
-    Write-Warning "Couldn't find a compatible install of Office"
-}
-
 Write-Progress "Hiding desktop icons"
 if ((Get-ItemProperty HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\).HideIcons -ne 1) {
     Set-ItemProperty HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\ -Name HideIcons -Value 1
@@ -98,6 +30,20 @@ if (-not (Test-Path c:\code)) {
 Write-Progress "Making c:\temp"
 if (-not (Test-Path c:\temp)) {
     New-Item c:\temp -ItemType Directory
+}
+
+Write-Progress "Symlinking Outlook Signatures to OneDrive:\Documents\Outlook\Signatures"
+if (Test-Path HKCU:\Software\Microsoft\Office\16.0) {
+    if (Test-Path "$($Env:AppData)\Microsoft\Signatures") {
+        Write-Warning "Signatures already exist. Not gonna do that."
+    }
+    else
+    {
+        New-Item -ItemType SymbolicLink -Path "$($Env:AppData)\Microsoft\Signatures" -Value "$($Env:UserProfile)\OneDrive\Documents\Outlook\Signatures"
+    }
+}
+else {
+    Write-Warning "Couldn't find a compatible install of Office"
 }
 
 Write-Progress "Enabling Windows Subsystem for Linux"
